0a18e3af77b8bb819e340ffdad10286040cfc192
f1r3manec <gabriel_cazar@hotmail.com>
2021-06-21 21:04:32 -0500

    https

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,5 +1,5 @@
 export const getGifts = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
   )}&limit=10&api_key=t8iOr2jE0ZfsBp6G22xJjWoAKorAsDwp`;
   const resp = await fetch(url);
